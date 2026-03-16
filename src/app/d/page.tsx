0016d31c5971569a64134f18261b86361822dbed
brunorwanda4 <brunorwanda4@gmail.com>
2026-03-16 16:54:51 +0200

    make dashboard overview chart

--- src/app/d/page.tsx
+++ src/app/d/page.tsx
@@ -1,9 +1,11 @@
 import DashboardTitle from "./_components/common/dashboard-title"
+import DashboardOverviewData from "./_components/dashboard/dashboard-overview-data"
 
 const DashboardPage = () => {
   return (
-    <div>
+    <div className=" space-y-4">
       <DashboardTitle title="Dashboard Overview" />
+      <DashboardOverviewData />
     </div>
   )
 }
