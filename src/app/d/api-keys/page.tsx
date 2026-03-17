338bea443af2ebcaab6c1465dbd2071c7132320c
brunorwanda4 <brunorwanda4@gmail.com>
2026-03-17 03:24:17 +0200

    Add api metrics overview

--- src/app/d/api-keys/page.tsx
+++ src/app/d/api-keys/page.tsx
@@ -1,5 +1,16 @@
+import ApiMetricsOverview from "../_components/api-keys/api-metrics-overview";
+import DashboardTitle from "../_components/common/dashboard-title";
+
 const ApiKeysPage = () => {
-  return <div>ApiKeysPage</div>;
+  return (
+    <div className=" space-y-8">
+      <DashboardTitle
+        desc="Manage API for system integration and external access"
+        title="API Keys Management"
+      />
+      <ApiMetricsOverview />
+    </div>
+  );
 };
 
 export default ApiKeysPage;
